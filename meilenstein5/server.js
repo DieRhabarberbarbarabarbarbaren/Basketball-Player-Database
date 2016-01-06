0f186d10b6079453fa6fe911469a0c1c3eb70040
xrounder <1414163@stud.hs-mannheim.de>
2016-01-06 03:50:31 +0100

    Added: form.txt, created for data from player_entry.html, task 5.2 - Miguel Changed: player_entry.html, changed destination for sent data to own webserver, task 5.2 - Miguel changed: server.js, added functionality to webserver and it shoud work fine now, task 5.2 - Miguel

--- meilenstein5/server.js
+++ meilenstein5/server.js
@@ -1,15 +1,52 @@
 var url = {
-     port : 8888,
-     ip : "127.0.01"
+    ip: '127.0.0.1',
+    port: 8888
 };
 var http = require('http');
+var filesystem = require("fs");
 
-http.createServer(function onRequest (request, response){
+http.createServer(function onRequest(request, response) {
+
+    var queryObject = parseRequest(request);
+
+    var fileURL = "form.txt";
+
+    saveInFile(fileURL, queryObject);
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Sie haben sich erfolgreich auf den WebServer mit der Url " + "\<" + url.port + ":" + url.ip + "\>" + " verbunden");
+    response.write("Sie haben sich erfolgreich auf den WebServer mit der Url " + "\<" + url.ip + ':' + url.port + "\>" + " verbunden");
     console.log("\<User connected to Server\>");
     response.end();
-}).listen(url.port);
+}).listen(url.port, url.ip);
 
+function parseRequest(request){
+
+    var query = request.url.substring(2);
+    var attributes = query.split('&');
+    var splittedAttributes;
+    var queryObject= new Object();
+    for(var i=0; i<attributes.length;i++){
+        splittedAttributes = attributes[i].split('=');
+        queryObject[splittedAttributes[0]] = splittedAttributes[1];
+    }
+
+    return queryObject;
+}
+
+function saveInFile(fileURL, queryObject) {
+
+    var savedQueryLine = queryObject.vorname + " "
+        + queryObject.name + ", "
+        + queryObject.jahr + ", "
+        + queryObject.hcoach + ", "
+        + queryObject.acoach + ", "
+        + queryObject.position + ", "
+        + queryObject.number + "\n";
+
+    filesystem.appendFile(fileURL, savedQueryLine, function (error) {
+        if (error) throw error;
+        console.log('It\'s saved!');
+    });
+
+}
 
 
