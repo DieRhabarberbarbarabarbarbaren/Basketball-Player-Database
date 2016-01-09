d8bd3528d97e187f79d008be8403ff525365763c
xrounder <1414163@stud.hs-mannheim.de>
2016-01-09 16:59:04 +0100

    changed directory structure. i copied all existing files from one milestone to the next milestone, so every milestone runs independently from the others - Miguel

---
+++ meilenstein5/js/server.js
@@ -0,0 +1,59 @@
+var url = {
+    ip: '127.0.0.1',
+    port: 8888
+};
+var http = require('http');
+var filesystem = require("fs");
+
+http.createServer(function onRequest(request, response) {
+
+    if(request.url == "/favicon.ico"){
+
+        response.writeHead(200);
+        response.end();
+    }else {
+        var queryObject = parseRequest(request);
+
+        var fileURL = "form.txt";
+
+        saveInFile(fileURL, queryObject);
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write("Sie haben sich erfolgreich auf den WebServer mit der Url " + "\<" + url.ip + ':' + url.port + "\>" + " verbunden");
+        console.log("\<User connected to Server\>");
+        response.end();
+    }
+}).listen(url.port, url.ip);
+
+function parseRequest(request){
+
+    var query = request.url.substring(2);
+    var attributes = query.split('&');
+    var splittedAttributes;
+    var queryObject= new Object();
+    for(var i=0; i<attributes.length;i++){
+        splittedAttributes = attributes[i].split('=');
+        queryObject[splittedAttributes[0]] = splittedAttributes[1];
+    }
+
+    return queryObject;
+}
+
+function saveInFile(fileURL, queryObject) {
+
+    if(queryObject != undefined) {
+        var savedQueryLine = queryObject.vorname + " "
+            + queryObject.name + ", "
+            + queryObject.jahr + ", "
+            + queryObject.hcoach + ", "
+            + queryObject.acoach + ", "
+            + queryObject.position + ", "
+            + queryObject.number + "\n";
+
+        filesystem.appendFile(fileURL, savedQueryLine, function (error) {
+            if (error) throw error;
+            console.log('It\'s saved!');
+        });
+    }
+}
+
+
